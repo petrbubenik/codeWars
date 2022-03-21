f4f8cb78290b37de038826938d43933da6addd34
Petr Bubenik <petrbubenik@gmail.com>
2022-03-21 18:00:02 +0000

    solved 8-kyu Invert values kata

---
+++ javascript/8kyu/invert_values.js
@@ -0,0 +1,7 @@
+//Invert values
+//https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/javascript
+
+function invert(array) {
+    let invertArray = array.map(x => -x)
+    return invertArray;
+  }
