cc74789720b18f43c5846a25f5a970b1ed41da7a
Petr Bubenik <petrbubenik@gmail.com>
2022-03-22 10:09:59 +0000

    solved 8kyu Convert boolean to string and DNA to RNA conversion katas

---
+++ javascript/8kyu/convert_boolean_to_string.js
@@ -0,0 +1,7 @@
+//Convert Boolean to String
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+function booleanToString(b){
+    let string = ""
+    return (b === true ? string = "true" : string = "false")
+}
---
+++ javascript/8kyu/dna_to_rna_conversion.js
@@ -0,0 +1,3 @@
+//DNA to RNA conversion
+//https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+
