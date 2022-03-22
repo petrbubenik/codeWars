91c326b2631faebada1d2e92b12b398dc4e4bf54
Petr Bubenik <petrbubenik@gmail.com>
2022-03-22 10:12:55 +0000

    solved 8kyu DNA to RNA conversion kata

--- javascript/8kyu/dna_to_rna_conversion.js
+++ javascript/8kyu/dna_to_rna_conversion.js
@@ -1,3 +1,8 @@
 //DNA to RNA conversion
 //https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
 
+function DNAtoRNA(dna) {
+    let rna = "";
+    for(i = 0; i < dna.length; i++){dna[i] === 'T' ? rna += 'U' : rna += dna[i]};
+    return rna;
+}
