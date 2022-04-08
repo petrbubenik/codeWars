0352caba7fb8cd281cfc75bbf09a85e58803db33
Petr Bubenik <petrbubenik@gmail.com>
2022-04-08 17:51:24 +0200

    solved 7kyu You are a square and List filtering katas

---
+++ javascript/7kyu/list_filtering.js
@@ -0,0 +1,6 @@
+//List filtering
+//https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+function filter_list(l) {
+    return l.filter(x => typeof(x) === 'number')
+  }
---
+++ javascript/7kyu/youre_a_square.js
@@ -0,0 +1,6 @@
+//You're a square!
+//https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+function isSquare(n){
+    return Number.isInteger(Math.sqrt(n))
+}
